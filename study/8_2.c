0050f474713ce9b7eb49af5de8dcdd7f25eb9e12
Yang Youngjin <41133006+ojin0611@users.noreply.github.com>
2018-11-19 11:03:10 +0900

    4:반복문 / 8:배열과 포인터

---
+++ study/8_2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+// 임의 3개의 숫자 중 max,mid,min 계산 by포인터 
+void line_up(double* pmax, double* pmid, double *pmin);
+
+int main()
+{
+	double max, mid, min;
+	printf("임의의 숫자 세 개를 입력하세요 : ");
+	scanf("%lf %lf %lf", &max, &mid, &min);
+	line_up(&max, &mid, &min);
+	printf("큰 수부터 차례로 출력하면 : %lf, %lf, %lf\n",
+	 max, mid, min);
+	return 0;
+}
+void line_up(double* pmax, double* pmid, double *pmin){
+	double Max, Mid, Min;
+	// Max 구하기 
+	Max = *pmax;
+	if (Max<=*pmid) Max=*pmid;
+	if (Max<=*pmin) Max=*pmin;
+	
+	// Min 구하기 
+	Min = *pmin;
+	if (Min>=*pmax) Min=*pmax;
+	if (Min>=*pmid) Min=*pmid;
+	
+	// Mid 구하기
+	Mid = *pmax + *pmid + * pmin - Max - Min; 
+	
+	// 주소에 바뀐 값 저장 
+	*pmax = Max;
+	*pmid = Mid;
+	*pmin = Min;
+	return 0;
+}
---
+++ study/8_3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//배열, 포인터로 평균 구하기 
+int main()
+{
+	double ary[]={1.5, 20.1, 16.4, 2.3, 3.5};
+	double sum=0;
+
+	int arysize=sizeof(ary)/sizeof(double);
+
+	int i;
+	for(i=0;i<arysize;i++){
+		sum=sum+ *(ary+i);
+		printf("i=%d, sum= %0.2f\n",i, sum);
+	}
+	printf("array size=%d, avg= %0.2f\n",arysize, sum/arysize);
+
+
+	return 0;
+}
+
+
+
